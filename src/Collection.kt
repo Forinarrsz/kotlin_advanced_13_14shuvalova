f2d8f076f9cf520b66b4deed05cfbee77c92f115
dazzes4545 <dazzes7619@gmail.com>
2026-02-04 15:06:09 +0300

    Lists in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -6,7 +6,7 @@ val newsolarsystem = arrayOf(
     "mercury", "venus", "mars", "earth","jupiter", "saturn", "uranus", "neptune", "pluto"
 )
 fun main(){
-    println(solarsystem[0])
+    /*println(solarsystem[0])
     println(solarsystem[1])
     println(solarsystem[2])
     println(solarsystem[3])
@@ -16,5 +16,28 @@ fun main(){
     println(solarsystem[7])
 
     solarsystem[3] = "Pandora"
-    println(solarsystem[3])
+    println(solarsystem[3])*/
+
+    val solarsystemlist = listOf("mercury", "venus", "mars", "earth","jupiter", "saturn", "uranus", "neptune", "pluto")
+    println(solarsystemlist.size)
+    println(solarsystemlist[2])
+    println(solarsystemlist.get(3))
+    println(solarsystemlist.indexOf("mars"))
+    println(solarsystemlist.indexOf("venus"))
+
+    for (planet in solarsystemlist) {
+        println(planet)
+    }
+
+    val solarsystemmutlist = mutableListOf("mercury", "venus", "mars", "earth","jupiter", "saturn", "uranus", "neptune", "pluto")
+    solarsystemmutlist.add("Pandora")
+    solarsystemmutlist.add(3, "theia")
+
+    solarsystemmutlist[3] = "moon"
+    println(solarsystemmutlist[3])
+    println(solarsystemmutlist[9])
+    solarsystemmutlist.removeAt(9)
+    println(solarsystemmutlist.contains("mars"))
+    println("Pandora" in solarsystemmutlist)
+
 }
