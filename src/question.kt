562685327ce74504aba90b9c893555728d9c5307
dazzes4545 <dazzes7619@gmail.com>
2026-02-04 14:09:48 +0300

    Use an enum class

--- src/question.kt
+++ src/question.kt
@@ -1,18 +1,22 @@
 class Question<T>(
     val questionText: String,
     val answer: T,
-    val difficulty: String
+    val difficulty: Difficulty
 )
-
+enum class Difficulty{
+    EASY,
+    MEDIUM,
+    HARD
+}
 fun main(){
     val qst1 = Question<String>(
-        "речка спятила с ума - по домам пошла сама","водопад","medium"
+        "речка спятила с ума - по домам пошла сама","водопад",Difficulty.MEDIUM
     )
     val qst2 = Question<Boolean>(
-        "небо зеленое. правда или ложь", false, "easy"
+        "небо зеленое. правда или ложь", false, Difficulty.EASY
     )
     val qst3 = Question<Int>(
-        "сколько дней между полнолуниями?",28,"hard"
+        "сколько дней между полнолуниями?",28,Difficulty.HARD
     )
 
     println(qst1.answer)
