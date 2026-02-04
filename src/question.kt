28d67376467cd673f42e2376ffc47bb9a19e4752
dazzes4545 <dazzes7619@gmail.com>
2026-02-04 14:23:18 +0300

    Add extension properties and functions

--- src/question.kt
+++ src/question.kt
@@ -5,33 +5,42 @@ data class Question<T>(
 )
 class Quiz {
     val qst1 = Question<String>(
-        "речка спятила с ума - по домам пошла сама","водопад",Difficulty.MEDIUM
+        "речка спятила с ума - по домам пошла сама", "водопад", Difficulty.MEDIUM
     )
     val qst2 = Question<Boolean>(
         "небо зеленое. правда или ложь", false, Difficulty.EASY
     )
     val qst3 = Question<Int>(
-        "сколько дней между полнолуниями?",28,Difficulty.HARD
+        "сколько дней между полнолуниями?", 28, Difficulty.HARD
     )
+
     companion object StudentProgress {
         var total: Int = 10
         var answered: Int = 3
     }
-
 }
+val Quiz.StudentProgress.progressText: String
+    get() = "$answered of $total answered"
+
 enum class Difficulty{
     EASY,
     MEDIUM,
     HARD
 }
+fun Quiz.StudentProgress.printProgressBar(){
+    repeat(Quiz.answered) { println("▓") }
+    repeat(Quiz.total - Quiz.answered) { println("▓") }
+    println()
+    println(Quiz.progressText)
+}
 
 fun main(){
-
-
+println(Quiz.progressText)
+Quiz.printProgressBar()
    /* println(qst1.answer)
     println(qst2.answer)
     println(qst3.answer)
     println(qst1.toString())
     println("========")*/
-    println("${Quiz.answered} of ${Quiz.total} answered")
+    //println("${Quiz.answered} of ${Quiz.total} answered")
 }
