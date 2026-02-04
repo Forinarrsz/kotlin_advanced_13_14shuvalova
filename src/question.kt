14527452178245d4287a492b9cc72689427a51ed
dazzes4545 <dazzes7619@gmail.com>
2026-02-04 14:44:00 +0300

    Add extension properties and functions

--- src/question.kt
+++ src/question.kt
@@ -3,44 +3,52 @@ data class Question<T>(
     val answer: T,
     val difficulty: Difficulty
 )
-class Quiz {
-    val qst1 = Question<String>(
-        "речка спятила с ума - по домам пошла сама", "водопад", Difficulty.MEDIUM
-    )
-    val qst2 = Question<Boolean>(
-        "небо зеленое. правда или ложь", false, Difficulty.EASY
-    )
-    val qst3 = Question<Int>(
-        "сколько дней между полнолуниями?", 28, Difficulty.HARD
-    )
+
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
+
+class Quiz : ProgressPrintable {
+
+//    val qst1 = Question<String>(
+//        "речка спятила с ума - по домам пошла сама", "водопад", Difficulty.MEDIUM
+//    )
+//    val qst2 = Question<Boolean>(
+//        "небо зеленое. правда или ложь", false, Difficulty.EASY
+//    )
+//    val qst3 = Question<Int>(
+//        "сколько дней между полнолуниями?", 28, Difficulty.HARD
+//    )
 
     companion object StudentProgress {
         var total: Int = 10
         var answered: Int = 3
     }
-}
-val Quiz.StudentProgress.progressText: String
-    get() = "$answered of $total answered"
+    override val progressText: String
+        get() = "$answered of $total answered"
 
-enum class Difficulty{
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { println("▓") }
+        repeat(Quiz.total - Quiz.answered) { println("▓") }
+        println()
+        println(progressText)
+    }
+}
+
+
+enum class Difficulty {
     EASY,
     MEDIUM,
     HARD
 }
-fun Quiz.StudentProgress.printProgressBar(){
-    repeat(Quiz.answered) { println("▓") }
-    repeat(Quiz.total - Quiz.answered) { println("▓") }
-    println()
-    println(Quiz.progressText)
-}
 
-fun main(){
-println(Quiz.progressText)
-Quiz.printProgressBar()
-   /* println(qst1.answer)
-    println(qst2.answer)
-    println(qst3.answer)
-    println(qst1.toString())
-    println("========")*/
+fun main() {
+    Quiz().printProgressBar()
+    /* println(qst1.answer)
+     println(qst2.answer)
+     println(qst3.answer)
+     println(qst1.toString())
+     println("========")*/
     //println("${Quiz.answered} of ${Quiz.total} answered")
 }
